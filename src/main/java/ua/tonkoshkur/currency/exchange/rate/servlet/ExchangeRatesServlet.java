205868d0c1ef09ca129e06bbb0258c8e045ec0b1
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-03-05 16:31:43 +0100

    feat: implement exchange rates servlet

--- src/main/java/ua/tonkoshkur/currency/exchange/rate/servlet/ExchangeRatesServlet.java
+++ src/main/java/ua/tonkoshkur/currency/exchange/rate/servlet/ExchangeRatesServlet.java
@@ -5,19 +5,44 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ua.tonkoshkur.currency.exchange.rate.dto.ExchangeRateDto;
+import ua.tonkoshkur.currency.exchange.rate.dto.SaveExchangeRateRequest;
+import ua.tonkoshkur.currency.exchange.rate.service.ExchangeRateService;
+import ua.tonkoshkur.currency.exchange.rate.servlet.mapper.RequestToSaveExchangeRateRequestMapper;
+import ua.tonkoshkur.currency.exchange.util.BeanFactory;
+import ua.tonkoshkur.currency.exchange.util.ResponseWriter;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
 
+    private final ExchangeRateService exchangeRateService;
+    private final RequestToSaveExchangeRateRequestMapper requestToSaveRequestMapper;
+
+    public ExchangeRatesServlet() {
+        this.exchangeRateService = BeanFactory.getExchangeRateService();
+        this.requestToSaveRequestMapper = new RequestToSaveExchangeRateRequestMapper();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<ExchangeRateDto> exchangeRates = exchangeRateService.findAll();
+
+        ResponseWriter.write(exchangeRates, resp);
+
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        SaveExchangeRateRequest saveRequest = requestToSaveRequestMapper.map(req);
+
+        ExchangeRateDto savedExchangeRate = exchangeRateService.save(saveRequest);
+
+        ResponseWriter.write(savedExchangeRate, resp);
+
+        resp.setStatus(HttpServletResponse.SC_CREATED);
     }
 }
---
+++ src/main/java/ua/tonkoshkur/currency/exchange/rate/servlet/mapper/RequestToSaveExchangeRateRequestMapper.java
@@ -0,0 +1,30 @@
+package ua.tonkoshkur.currency.exchange.rate.servlet.mapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import ua.tonkoshkur.currency.exchange.exception.BadRequestException;
+import ua.tonkoshkur.currency.exchange.rate.dto.SaveExchangeRateRequest;
+import ua.tonkoshkur.currency.exchange.rate.servlet.extractor.RateValueExtractor;
+import ua.tonkoshkur.currency.exchange.util.RequestMapper;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class RequestToSaveExchangeRateRequestMapper implements RequestMapper<SaveExchangeRateRequest> {
+
+    @Override
+    public SaveExchangeRateRequest map(HttpServletRequest request) throws BadRequestException, IOException {
+        String baseCurrencyCode = request.getParameter("baseCurrencyCode");
+        String targetCurrencyCode = request.getParameter("targetCurrencyCode");
+        if (baseCurrencyCode == null) {
+            throw new BadRequestException("Base currency code is required");
+        }
+        if (targetCurrencyCode == null) {
+            throw new BadRequestException("Target currency code is required");
+        }
+
+        String rateParam = request.getParameter("rate");
+        BigDecimal rate = RateValueExtractor.extract(rateParam);
+
+        return new SaveExchangeRateRequest(baseCurrencyCode, targetCurrencyCode, rate);
+    }
+}
